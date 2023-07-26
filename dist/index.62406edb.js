9f0a4805395227c8a8f1cd428c4a54a61e8c1c98
Izabelruseva <rusevaizabel@gmail.com>
2023-07-26 10:04:46 +0300

    .

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -5,6 +5,24 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
+};
+const controlRecipes = async function() {
+    try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+        recipeView.renderSpinner();
+        // 0) Update results view to mark selected search result
+        resultsView.update(model.getSearchResultsPage());
+        // 1) Updating bookmarks view
+        bookmarksView.update(model.state.bookmarks);
+        // 2) Loading recipe
+        await model.loadRecipe(id);
+        // 3) Rendering recipe
+        recipeView.render(model.state.recipe);
+    } catch (err) {
+        recipeView.renderError();
+        console.error(err);
+    }
 }; // https://forkify-api.herokuapp.com/v2
  ///////////////////////////////////////
 
--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,29 @@ const timeout = function (s) {
   });
 };
 
+const controlRecipes = async function () {
+    try {
+      const id = window.location.hash.slice(1);
+  
+      if (!id) return;
+      recipeView.renderSpinner();
+  
+      // 0) Update results view to mark selected search result
+      resultsView.update(model.getSearchResultsPage());
+  
+      // 1) Updating bookmarks view
+      bookmarksView.update(model.state.bookmarks);
+  
+      // 2) Loading recipe
+      await model.loadRecipe(id);
+  
+      // 3) Rendering recipe
+      recipeView.render(model.state.recipe);
+    } catch (err) {
+      recipeView.renderError();
+      console.error(err);
+    }
+  };
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
